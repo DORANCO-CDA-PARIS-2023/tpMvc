2c8058c2fae12b8b9c886632f315752e4ecdd5fb
imane-R <imanebenamar22@gmail.com>
2024-03-19 16:59:50 +0100

    tpMVC

---
+++ src/main/java/com/doranco/coursSpring/controller/Connexion.java
@@ -0,0 +1,8 @@
+package com.doranco.coursSpring.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Connexion {
+
+}
--- src/main/java/com/doranco/coursSpring/model/entity/User.java
+++ src/main/java/com/doranco/coursSpring/model/entity/User.java
@@ -5,13 +5,18 @@ public class User {
     private String lastName;
     private String firstName;
     private String email;
+    private String password;
 
-    public User() {}
+    private Set<String> roles = new HashSet<>();
 
-    public User(String lastName, String firstName, String email) {
+    public User() {
+    }
+
+    public User(String lastName, String firstName, String email, String password,) {
         this.lastName = lastName;
         this.firstName = firstName;
         this.email = email;
+        this.password = password;
     }
 
     public String getLastName() {
@@ -38,12 +43,21 @@ public class User {
         this.email = email;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "lastName='" + lastName + '\'' +
                 ", firstName='" + firstName + '\'' +
                 ", email='" + email + '\'' +
+                ", password='" + password +
                 '}';
     }
 }
