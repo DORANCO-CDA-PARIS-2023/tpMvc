3a366bb506c984b832f805ba355097ed3c956d7a
Clement Gautriaud <cgautriaud@orange.fr>
2024-03-19 12:32:21 +0100

    pause

--- src/main/java/com/doranco/coursSpring/controller/ArticleController.java
+++ src/main/java/com/doranco/coursSpring/controller/ArticleController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -30,7 +31,8 @@ public class ArticleController {
     }
 
     @PostMapping("/article")
-    public String addArticle() {
+    public String addArticle(@ModelAttribute Article article, Model model) {
+        model.addAttribute("article", article);
         return "index";
     }
 
