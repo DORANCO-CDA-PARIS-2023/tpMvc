8d2009794a4b5e391bb822f23049c4683f553a2b
William T. <william.tahar@gmail.com>
2024-03-21 17:03:04 +0100

    Correction TP API

--- src/main/java/com/doranco/coursSpring/controller/api/ArticleApiController.java
+++ src/main/java/com/doranco/coursSpring/controller/api/ArticleApiController.java
@@ -1,12 +1,19 @@
 package com.doranco.coursSpring.controller.api;
 
+import com.doranco.coursSpring.controller.api.payload.Payload;
 import com.doranco.coursSpring.model.entity.Article;
 import com.doranco.coursSpring.repository.ArticleRepository;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.http.HttpResponse;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
+@RequestMapping("/api")
 public class ArticleApiController {
 
     private final ArticleRepository articleRepository;
@@ -16,33 +23,65 @@ public class ArticleApiController {
         this.articleRepository = articleRepository;
     }
 
-    @GetMapping("/api/article")
-    public List<Article> getAll()
+    @GetMapping("/article")
+    public ResponseEntity<Payload> getAll()
     {
-        return articleRepository.findAll();
+        var articles = articleRepository.findAll();
+        Payload payload = new Payload("Get All article", articles);
+        if (articles.isEmpty()) {
+            return new ResponseEntity<>(payload, HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(payload, HttpStatus.OK);
     }
 
-    @GetMapping("/api/article/{id}")
-    public Article getById(int id)
+    @GetMapping("/article/{id}")
+    public ResponseEntity<Payload> getById(@PathVariable int id)
     {
-        return null;
+        Payload payload = new Payload();
+        try {
+            var article = articleRepository.findById(id).get();
+            payload.setMessage("Get one Article");
+            payload.setContent(article);
+            return new ResponseEntity<>(payload, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            payload.setMessage("Article not found");
+            return new ResponseEntity<>(payload, HttpStatus.NOT_FOUND);
+        }
     }
 
-    @PostMapping("/api/article")
-    public Article add(/* TODO */)
+    @PostMapping("/article")
+    public ResponseEntity<Payload> add(@RequestBody Article article)
     {
-        return null;
+        Payload payload = new Payload();
+        article = articleRepository.save(article);
+        payload.setContent(article);
+        payload.setMessage("Article created");
+        return new ResponseEntity<>(payload, HttpStatus.CREATED);
     }
 
 
-    @PutMapping("/api/article/{id}")
-    public Article update(/* TODO */)
+    @PutMapping("/article/{id}")
+    public ResponseEntity<Payload> update(@RequestBody Article formArticle, @PathVariable int id)
     {
-        return null;
+        Payload payload = new Payload();
+        try {
+            var article = articleRepository.findById(id).get();
+            article.setTitle(formArticle.getTitle());
+            article.setContent(formArticle.getContent());
+            var articleUpdated = articleRepository.save(article);
+
+            payload.setContent(articleUpdated);
+            payload.setMessage("Article updated");
+            return new ResponseEntity<>(payload, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            payload.setMessage("Article ID (" + id + ") NOT FOUND");
+            return new ResponseEntity<>(payload, HttpStatus.NOT_FOUND);
+        }
     }
 
-    @DeleteMapping("/api/article/{id}")
-    public String deleteById(int id) {
-        return "";
+    @DeleteMapping("/article/{id}")
+    public ResponseEntity<Payload> deleteById(@PathVariable int id) {
+        articleRepository.deleteById(id);
+        return new ResponseEntity<>(new Payload("Article ID : " + id + " deleted"), HttpStatus.OK);
     }
 }
