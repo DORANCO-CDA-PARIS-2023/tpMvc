ee9fde45e2016b66f7d31f7e8adde3910b5bde29
Ryan Sifi <ryan.sifi.77@gmail.com>
2024-03-21 15:31:38 +0100

    Avancement

--- src/main/java/com/doranco/coursSpring/controller/api/ArticleApiController.java
+++ src/main/java/com/doranco/coursSpring/controller/api/ArticleApiController.java
@@ -2,9 +2,12 @@ package com.doranco.coursSpring.controller.api;
 
 import com.doranco.coursSpring.model.entity.Article;
 import com.doranco.coursSpring.repository.ArticleRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ArticleApiController {
@@ -23,26 +26,41 @@ public class ArticleApiController {
     }
 
     @GetMapping("/api/article/{id}")
-    public Article getById(int id)
+    public ResponseEntity<Article> getById(@PathVariable int id)
     {
-        return null;
+        Optional<Article> article = articleRepository.findById(id);
+        return article.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/api/article")
-    public Article add(/* TODO */)
-    {
-        return null;
+    public ResponseEntity<Article> add(@RequestBody Article article) {
+        Article saveArticle = articleRepository.save(article);
+        return new ResponseEntity<>(saveArticle, HttpStatus.CREATED);
     }
 
 
     @PutMapping("/api/article/{id}")
-    public Article update(/* TODO */)
-    {
-        return null;
+    public ResponseEntity<Article> update(@PathVariable int id, @RequestBody Article updatedArticle) {
+        Optional<Article> optionalArticle = articleRepository.findById(id);
+        if (optionalArticle.isPresent()) {
+            Article currentArticle = optionalArticle.get();
+            currentArticle.setTitle(updatedArticle.getTitle());
+            currentArticle.setContent(updatedArticle.getContent());
+            articleRepository.save(currentArticle);
+            return new ResponseEntity<>(currentArticle, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/api/article/{id}")
-    public String deleteById(int id) {
-        return "";
+    public ResponseEntity<String> deleteById(@PathVariable int id) {
+        Optional<Article> article = articleRepository.findById(id);
+        if (article.isPresent()) {
+            articleRepository.deleteById(id);
+            return new ResponseEntity<>("Article correctement supprimer", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Erreur article non supprimer", HttpStatus.NOT_FOUND);
+        }
     }
 }
